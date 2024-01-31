827eaeeaf909e715dde9f5fb85219cd436b480fa
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-31 17:17:35 -0300

    feat: :construction: Add Home page with coffee display

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import { Banner } from './Banner'
+import { coffes } from './Coffees'
+import { FoodCard } from './FoodCard'
+
+export function Home() {
+  return (
+    <main>
+      <Banner />
+      <main className="h-screen w-screen px-40 py-8">
+        <h2 className="mb-14 text-3xl font-bold text-base-subtitle">
+          Nossos Cafes
+        </h2>
+        <article className="grid grid-cols-4 grid-rows-4 gap-8">
+          {coffes.map((coffe) => (
+            <FoodCard key={coffe.title} coffe={coffe} />
+          ))}
+        </article>
+      </main>
+    </main>
+  )
+}
