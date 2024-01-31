f73327a225c8462f39c85c50acaf1a2b21ffad85
Fabio Fiorita <fabiolfp@gmail.com>
2024-01-31 17:17:44 -0300

    feat: :construction: Add NavBar and Home components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { NavBar } from './components/Navbar'
+import { Home } from './pages/Home'
+
 export function App() {
   return (
     <>
-      <div className="flex flex-col">
-        <p>Hello</p>
-        <p>Hello</p>
+      <div className="h-screen bg-base-background">
+        <NavBar />
+        <Home />
       </div>
     </>
   )
