53cb091b07f576dadcc07e0e02016503decac0b3
Fabio Fiorita <fabiolfp@gmail.com>
2024-02-01 16:56:18 -0300

    feat: :sparkles: Add navigation links to the logo and cart button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,23 @@
 import { MapPin } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import { CartButton } from '../CartButton'
 
 export function NavBar() {
   return (
     <header className="flex items-center justify-between px-40 py-8">
-      <img src={logo} alt="" />
+      <NavLink to="/" title="Início">
+        <img src={logo} alt="" />
+      </NavLink>
       <div>
         <div className="flex gap-3">
-          <button className="bg-purple-light text-purple-dark flex items-center justify-center gap-1 rounded p-2 text-sm hover:bg-purple-200">
+          <button className="flex items-center justify-center gap-1 rounded bg-purple-light p-2 text-sm text-purple-dark hover:bg-purple-200">
             <MapPin size={22} weight="fill" />
             <span>Porto Alegre, RS</span>
           </button>
-          <CartButton variant="secondary" />
+          <NavLink to="/order" title="Pedido">
+            <CartButton variant="secondary" />
+          </NavLink>
         </div>
       </div>
     </header>
