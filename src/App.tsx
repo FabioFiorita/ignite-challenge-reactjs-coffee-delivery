56e9f4a217da2429b6971c00aa1ba1bdfe49e757
Fabio Fiorita <fabiolfp@gmail.com>
2024-02-02 16:09:54 -0300

    feat: :sparkles: Add OrderProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { OrderProvider } from './contexts/OrderContext'
 
 export function App() {
   return (
     <>
       <div className="h-screen bg-base-background">
         <BrowserRouter>
-          <Router />
+          <OrderProvider>
+            <Router />
+          </OrderProvider>
         </BrowserRouter>
       </div>
     </>
