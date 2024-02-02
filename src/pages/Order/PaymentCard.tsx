da55d36453aa326eaf22e6c2fda335ed95e9242d
Fabio Fiorita <fabiolfp@gmail.com>
2024-02-02 16:09:04 -0300

    feat: :sparkles: Add payment method selection functionality

--- src/pages/Order/PaymentCard.tsx
+++ src/pages/Order/PaymentCard.tsx
@@ -1,6 +1,10 @@
 import { Bank, CreditCard, CurrencyDollar, Money } from 'phosphor-react'
+import { useContext } from 'react'
+import { OrderContext, PaymentMethod } from '../../contexts/OrderContext'
 
 export function PaymentCard() {
+  const { paymentMethod, handlePaymentMethodChange } = useContext(OrderContext)
+
   return (
     <div className="rounded bg-base-card p-10 shadow">
       <div className="mb-8 flex gap-3">
@@ -13,20 +17,30 @@ export function PaymentCard() {
         </div>
       </div>
       <div className="flex gap-3">
-        <button>
-          <div className="flex items-center justify-center gap-3 rounded bg-base-button p-4 hover:bg-slate-300">
+        <button
+          onClick={() => handlePaymentMethodChange(PaymentMethod.CREDIT_CARD)}
+        >
+          <div
+            className={`flex items-center justify-center gap-3 rounded ${paymentMethod === PaymentMethod.CREDIT_CARD ? 'bg-purple-300' : 'bg-base-button'}  p-4 hover:bg-purple-100`}
+          >
             <CreditCard className="text-purple-base" size={16} />
             <span className="uppercase text-base-text">Cartão de crédito</span>
           </div>
         </button>
-        <button>
-          <div className="flex items-center justify-center gap-3 rounded bg-base-button p-4 hover:bg-slate-300">
+        <button
+          onClick={() => handlePaymentMethodChange(PaymentMethod.DEBIT_CARD)}
+        >
+          <div
+            className={`flex items-center justify-center gap-3 rounded ${paymentMethod === PaymentMethod.DEBIT_CARD ? 'bg-purple-300' : 'bg-base-button'}  p-4 hover:bg-purple-100`}
+          >
             <Bank className="text-purple-base" size={16} />
             <span className="uppercase text-base-text">cartão de débito</span>
           </div>
         </button>
-        <button>
-          <div className="flex items-center justify-center gap-3 rounded bg-base-button p-4 hover:bg-slate-300">
+        <button onClick={() => handlePaymentMethodChange(PaymentMethod.CASH)}>
+          <div
+            className={`flex items-center justify-center gap-3 rounded ${paymentMethod === PaymentMethod.CASH ? 'bg-purple-300' : 'bg-base-button'}  p-4 hover:bg-purple-100`}
+          >
             <Money className="text-purple-base" size={16} />
             <span className="uppercase text-base-text">dinheiro</span>
           </div>
