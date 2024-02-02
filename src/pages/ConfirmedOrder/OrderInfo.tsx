fd8ba4aa1d5a53133d738a6faf4194bf005d9c38
Fabio Fiorita <fabiolfp@gmail.com>
2024-02-02 16:08:17 -0300

    feat: :sparkles: Refactor OrderInfo component to use OrderContext

--- src/pages/ConfirmedOrder/OrderInfo.tsx
+++ src/pages/ConfirmedOrder/OrderInfo.tsx
@@ -1,9 +1,12 @@
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import { useContext } from 'react'
 import { Label, LabelContent, LabelPrefix } from '../../components/Label'
+import { OrderContext } from '../../contexts/OrderContext'
 
 export function OrderInfo() {
+  const { address, paymentMethod } = useContext(OrderContext)
   return (
-    <article className="min-w-128 flex flex-col items-start justify-center gap-8 rounded-bl-3xl rounded-tr-3xl border border-purple-base p-10">
+    <article className="flex min-w-128 flex-col items-start justify-center gap-8 rounded-bl-3xl rounded-tr-3xl border border-purple-base p-10">
       <Label>
         <LabelPrefix className="bg-purple-base">
           <MapPin size={16} weight="fill" />
@@ -12,9 +15,9 @@ export function OrderInfo() {
           <div>
             <p>
               Entrega em{' '}
-              <span className="font-bold">Rua João Daniel Martinelli, 102</span>
+              <span className="font-bold">{`${address.street}, ${address.number} ${address.complement ? `- ${address.complement}` : ''}`}</span>
             </p>
-            <p>Farrapos - Porto Alegre, RS</p>
+            <p>{`${address.neighborhood}, ${address.city} - ${address.state}`}</p>
           </div>
         </LabelContent>
       </Label>
@@ -36,7 +39,7 @@ export function OrderInfo() {
         <LabelContent>
           <div>
             <p>Pagamento na entrega</p>
-            <p className="font-bold">Cartão de Crédito</p>
+            <p className="font-bold">{paymentMethod}</p>
           </div>
         </LabelContent>
       </Label>
